80d0dcae2217d2cd76658a6134dde7873e4ad62a
haris49 <hariis.459@gmail.com>
2025-11-08 20:47:43 +0700

    Menambahkan judul "Data Peserta" dengan styling teks tebal dan warna putih

--- app/src/main/java/com/example/questnavigasitugas/view/MainApp.kt
+++ app/src/main/java/com/example/questnavigasitugas/view/MainApp.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -38,3 +41,11 @@ fun TampilData(navController: NavController) {
             )
             .padding(16.dp)
     ) {
+        Text(
+            "📋 Data Peserta",
+            style = MaterialTheme.typography.headlineSmall.copy(
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }

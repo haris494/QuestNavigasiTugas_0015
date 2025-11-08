202dcc12d80101357b6bb20cf4e904745f91c0d2
haris49 <hariis.459@gmail.com>
2025-11-08 21:14:01 +0700

    Menambahkan variabel DarkBg sebagai warna latar belakang kartu GlowCard

--- app/src/main/java/com/example/questnavigasitugas/view/IsiData.kt
+++ app/src/main/java/com/example/questnavigasitugas/view/IsiData.kt
@@ -31,6 +31,7 @@ import androidx.navigation.NavController
 
 private val NeonBlue   = Color(0xFF4F46E5)
 private val NeonPurple = Color(0xFF7C3AED)
+private val DarkBg     = Color(0xFF0F172A)
 
 data class Peserta(
     val nama: String,

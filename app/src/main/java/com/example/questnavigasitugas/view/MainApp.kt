44f0450ca222251071baf9f21481dc2991593654
haris49 <hariis.459@gmail.com>
2025-11-08 20:44:51 +0700

    Menambahkan tampilan daftar peserta pada fungsi TampilData

--- app/src/main/java/com/example/questnavigasitugas/view/MainApp.kt
+++ app/src/main/java/com/example/questnavigasitugas/view/MainApp.kt
@@ -1,6 +1,23 @@
 package com.example.questnavigasitugas.view
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
 
 private val NeonBlue   = Color(0xFF4F46E5)
 private val NeonPurple = Color(0xFF7C3AED)
+
+data class Peserta(
+    val nama: String,
+    val jk: String,
+    val status: String,
+    val alamat: String
+)
+
+@Composable
+fun TampilData(navController: NavController) {
+    val pesertaList = listOf(
+        Peserta("Aditia Rahman", "Laki-laki", "Belum Menikah", "Sleman"),
+        Peserta("Bagus Saputra", "Laki-laki", "Belum Menikah", "Yogyakarta"),
+        Peserta("Nadya Amalia", "Perempuan", "Belum Menikah", "Bantul")
+    )
